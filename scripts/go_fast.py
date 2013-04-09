c50449feecc7dadfd0866e84340a8ab7743e0673
Eric Schneider <franz.eric.schneider@gmail.com>
2013-04-09 12:21:27 -0400

    Did a first round on go_fast think code and put a placeholder in for mission.py

--- scripts/go_fast.py
+++ scripts/go_fast.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('olinoboat')
 from hardware import sensors
-from hardware import servos
 import rospy
+from std_msgs.msg import String
 
-def update_heading_on_wind_direction_change(angle):
+def go_fast_cb(angle):
+    
+	go_fast_pub = rospy.Publisher('go_fast_topic', String)
     rospy.loginfo("Wind sensor sent %i" % (angle))
 
-    compass_angle = sensors.compass.compass_angle
-    rospy.loginfo("compass says:"+compass_angle)
-    rel_angle = angle.data - compass_angle
-    rel_angle = abs((rel_angle + 180) % 360 - 180)
+    upwind_cutoff = 50	    # Degrees
+    downwind_cutoff = 10    # Degrees
 
-    points_of_sail = [0, 45, 60, 90, 135, 180]    
-    sail_points = [0, 0, 15, 40, 60, 80]
-    sail_angle = pl.interp(rel_angle, points_of_sail, sail_angle)
-    rospy.loginfo("heading angle should be:" + sail_angle)
+    # This code makes it so that you can use pl.interp on relative_angles and go_or_not_go and it will return '0' if the given angle is directly downwind or in irons
+    relative_angles = [0, upwind_cutoff - 1, upwind_cutoff, 180-downwind_cutoff-1, 180-downwind_cutoff, 180+downwind_cutoff, 180+downwind_cutoff+1, -upwind_cutoff-1, -upwind_cutoff, 0]
+    relative_angles = relative_angles + angle
+    for i in range(len(relative_angles))
+    	relative_angles[i] = relative_angles[i] % 360
+    go_or_not_go = [0, 0, 1, 1, 0, 0, 1, 1, 0]
+
+    # Builds a list of 360 degree elements around the boat that say whether the boat can go in a given direction
+    # If the 30th number in the list is 0, it means the boat shouldn't go 30 degrees to the right
+    for i in range(360):
+    	go_fast_heading[i] = pl.interp(i, relative_angles, go_or_not_go)
+
+    go_fast_pub.publish(str(go_fast_heading))
+    rospy.loginfo("go_fast_node suggested heading is:" + str(go_fast_heading)
 
 
-current_node = rospy.init_node("go_fast",anonymous=True)
+current_node = rospy.init_node("go_fast_node",anonymous=True)
 sensors.init(current_node)
 
-sensors.wind_angle.set_callback(update_heading_on_wind_direction_change)
+sensors.wind_angle.set_callback(go_fast_cb)
 
 rospy.spin()
---
+++ scripts/hardware/mission.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('olinoboat')
+import rospy
+from std_msgs.msg import UInt16
+
+compass=None
+gps=None
+wind_angle=None
+leak_detector=None
+
+class WindAngle():
+
+    def __init__(self, offset = 0, node = 0):
+        self.__offset = offset
+        self.angle = 0
+        self.callback = self.__default_callback
+
+        #initialize node if not running from the think code
+        if node == 0:
+            rospy.init_node('data_listener', anonymous=False)
+
+        #set up subscribers to ("topic", DataType, callback_function)
+        rospy.Subscriber("offset", UInt16, self.__set_offset)
+        rospy.Subscriber("pwm_duration", UInt16, self.__pwm_to_wind_angle)
+        rospy.loginfo("WindAngle initialized")
+
+    def __set_offset(self, data):
+        rospy.loginfo("encoder send offset signal: %i" % (data.data))
+        self.offset = data.data
+
+    def __pwm_to_wind_angle(self, data):
+        rospy.loginfo("encoder sent pwm signal: %i" % (data.data))
+        phigh = data.data
+        self.angle = (360*(phigh - self.__offset)/1024.)%360
+        self.callback(self.angle)
+
+    def set_callback(self,callback):
+        self.callback = callback
+
+    def __default_callback(self, angle):
+        rospy.loginfo("encoder sent pwm signal, but no additional callback has been registered" % (data.data))
+
+
+class GPS():
+
+    def __init__(self, node = 0):
+        self.current_location = [0,0,0]
+        self.callback = self.__default_callback
+
+        #initialize node if not loading from think code
+        if node == 0:
+            rospy.init_node('data_listener', anonymous=False)
+
+        rospy.Subscriber("gps_lat", UInt16, self.__set_current_lat)
+        rospy.Subscriber("gps_lon", UInt16, self.__set_current_lon)
+        rospy.loginfo("GPS initialized")
+
+    def __set_current_lat(self, data):
+        rospy.loginfo("GPS sent %i latitude" % (data.data))
+        self.current_lat = data.data
+        self.callback(self.current_lat)
+
+    def __set_current_lon(self, data):
+        rospy.loginfo("GPS sent %i longitude" % (data.data))
+        self.current_lon = data.data
+        self.callback(self.current_lon)
+
+    def set_callback(self,callback):
+        self.callback = callback
+
+    def __default_callback(self, position):
+        rospy.loginfo("GPS sent data, but no additional callback has been registered" % (data.data))
+
+
+class Compass():
+
+    def __init__(self, node = 0): 
+        self.callback = self.__default_callback          
+    
+        #initialize node if not loading from think code     
+        if node == 0:
+            rospy.init_node('data_listener', anonymous=False)
+    
+        rospy.Subscriber("heading", UInt16, self.__set_angle)
+        rospy.loginfo("Compass initialized")
+
+    def __set_angle(self, data):
+        rospy.loginfo("Compass sent %i" % (data.data))
+        self.compass_angle = data.data
+        self.callback(self.compass_angle)
+
+    def set_callback(self,callback):
+        self.callback = callback
+
+    def __default_callback(self, direction):
+        rospy.loginfo("encoder sent pwm signal, but no additionalcallback has been registered" % (data.data))
+
+class LeakDetector():
+    def __init__(self, node=0, leak_callback=0,):
+        self.leak_detected = 0 
+        self.leak_callback = leak_callback or self.__default_leak_callback  
+
+        if node == 0:
+            rospy.init_node('data_listener', anonymous=False)
+
+        rospy.Subscriber("leak",UInt16,self.__check_for_leak)
+        rospy.loginfo("Leak detector initialized")
+
+    def __check_for_leak(self,data):
+        if data.data > 1:
+            self.leak_detected = 1
+            self.leak_callback()
+        else:
+            rospy.loginfo("clear, no leak")
+            self.leak = 0
+
+    def __default_leak_callback(self):
+        rospy.loginfo("Leak detected, but no callback registered")
+
+    def set_callback(self,callback):
+        self.leak_callback = callback
+
+
+def init(node):
+    global wind_angle
+    global gps
+    global compass
+    global leak_detector
+
+    wind_angle = WindAngle(0,node)
+    gps = GPS(node)
+    compass = Compass(node)
+    leak_detector = LeakDetector(node)
+
+ 
+if __name__ == '__main__':
+   pass
