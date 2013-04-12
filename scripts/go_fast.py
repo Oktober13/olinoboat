bb777f6b86b5ffd21e966d760d9b7db70aeef04e
David N Gaynor <dgaynor@ubuntu.(none)>
2013-04-12 08:19:20 -0700

    make go_fast code importable

--- scripts/go_fast.py
+++ scripts/go_fast.py
@@ -5,6 +5,8 @@ import pylab as pl
 from hardware import sensors
 from std_msgs.msg import String
 
+suggested_heading = [1]*360
+
 def inhibit_bad_headings(inhibited_array, wind_heading):     # This array expects the form [[p1, p2], [p1, p2]...]
     print inhibited_array
     for i in range(len(inhibited_array)):
@@ -13,7 +15,8 @@ def inhibit_bad_headings(inhibited_array, wind_heading):     # This array expect
     return wind_heading
 
 def go_fast_cb():
-    
+    global suggested_heading
+
     angle = sensors.wind_angle.angle
 
     go_fast_pub = rospy.Publisher('go_fast_topic', String)
@@ -37,13 +40,15 @@ def go_fast_cb():
         go_fast_heading_weights = inhibit_bad_headings([[((angle+180)%360)-downwind_cutoff, ((angle+180)%360)+downwind_cutoff],\
             [angle-upwind_cutoff, angle+upwind_cutoff]], go_fast_heading_weights)
 
-    go_fast_pub.publish(str(go_fast_heading_weights))
+    # go_fast_pub.publish("go_fast:"+str(go_fast_heading_weights))
+    suggested_heading = go_fast_heading_weights
     rospy.loginfo("go_fast_node suggested heading is:" + str(go_fast_heading_weights))
 
 
-current_node = rospy.init_node("go_fast_node",anonymous=True)
-sensors.init(current_node)
+# current_node = rospy.init_node("go_fast_node",anonymous=True)
+# sensors.init(current_node)
 
-sensors.wind_angle.set_callback(go_fast_cb)
 
-rospy.spin()
+def init(think_node):
+    sensors.init(think_node)
+    sensors.wind_angle.set_callback(go_fast_cb)
---
+++ scripts/think.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('olinoboat')
+import rospy
+import go_fast
+import time
+
+think_node = rospy.init_node("think")
+
+go_fast.init(think_node)
+
+while not rospy.is_shutdown():
+	time.sleep(5)
+	rospy.loginfo("go_fast_node suggested heading is:")
