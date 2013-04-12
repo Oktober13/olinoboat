6648a08c4b276537f4bb6ebce63e9a9745dd22bb
David N Gaynor <dgaynor@ubuntu.(none)>
2013-04-11 22:28:50 -0700

    just edting think.py

--- scripts/go_fast.py
+++ scripts/go_fast.py
@@ -5,12 +5,12 @@ import pylab as pl
 from hardware import sensors
 from std_msgs.msg import String
 
-def pull_heading_up(array, heading):     # This array expects the form [[p1, p2], [p1, p2]...]
-    print array
-    for i in range(len(array)):
-        for j in range(int(round(array[i][0])), int(round(array[i][1]))):
-            heading[j] = 0
-    return heading
+def inhibit_bad_headings(inhibited_array, wind_heading):     # This array expects the form [[p1, p2], [p1, p2]...]
+    print inhibited_array
+    for i in range(len(inhibited_array)):
+        for j in range(int(round(inhibited_array[i][0])), int(round(inhibited_array[i][1]))):
+            wind_heading[j] = 0
+    return wind_heading
 
 def go_fast_cb():
     
@@ -22,23 +22,23 @@ def go_fast_cb():
     upwind_cutoff = 50	    # Degrees
     downwind_cutoff = 10    # Degrees
 
-    go_fast_heading = [1 for x in range(360)]
+    go_fast_heading_weights = [1 for x in range(360)]
 
     if (angle-upwind_cutoff) < 0 or (angle+upwind_cutoff) > 360:
         print 'Case 1'
-        go_fast_heading = pull_heading_up([[0, (angle+upwind_cutoff) % 360],[(angle-upwind_cutoff)%360, 360],\
-            [((angle+180)%360)-downwind_cutoff, ((angle+180)%360)+downwind_cutoff]], go_fast_heading)
+        go_fast_heading_weights = inhibit_bad_headings([[0, (angle+upwind_cutoff) % 360],[(angle-upwind_cutoff)%360, 360],\
+            [((angle+180)%360)-downwind_cutoff, ((angle+180)%360)+downwind_cutoff]], go_fast_heading_weights)
     elif (((angle-180)%360)-downwind_cutoff) < 0 or (((angle+180)%360)+downwind_cutoff) > 360:
         print 'Case 2'
-        go_fast_heading = pull_heading_up([[0, (angle+180+downwind_cutoff) % 360],[(angle-180-downwind_cutoff)%360, 360],\
-            [angle-upwind_cutoff, angle+upwind_cutoff]], go_fast_heading)
+        go_fast_heading_weights = inhibit_bad_headings([[0, (angle+180+downwind_cutoff) % 360],[(angle-180-downwind_cutoff)%360, 360],\
+            [angle-upwind_cutoff, angle+upwind_cutoff]], go_fast_heading_weights)
     else:
         print 'Case 3'
-        go_fast_heading = pull_heading_up([[((angle+180)%360)-downwind_cutoff, ((angle+180)%360)+downwind_cutoff],\
-            [angle-upwind_cutoff, angle+upwind_cutoff]], go_fast_heading)
+        go_fast_heading_weights = inhibit_bad_headings([[((angle+180)%360)-downwind_cutoff, ((angle+180)%360)+downwind_cutoff],\
+            [angle-upwind_cutoff, angle+upwind_cutoff]], go_fast_heading_weights)
 
-    go_fast_pub.publish(str(go_fast_heading))
-    rospy.loginfo("go_fast_node suggested heading is:" + str(go_fast_heading))
+    go_fast_pub.publish(str(go_fast_heading_weights))
+    rospy.loginfo("go_fast_node suggested heading is:" + str(go_fast_heading_weights))
 
 
 current_node = rospy.init_node("go_fast_node",anonymous=True)
