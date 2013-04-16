d205fb4af1b4a1862ce3fed2080d5251e63d4572
Eric Schneider <franz.eric.schneider@gmail.com>
2013-04-16 11:18:16 -0400

    Comnmented think.py

--- scripts/think.py
+++ scripts/think.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
+
+# think.py creates a loop that calls other code
+#
+# Dependent on : go_fast.py, go_short.py
+#		think.py takes the outputs of go_fast and go_short, multiplies them together, and then chooses the strongest element as the heading to go to
+#		See documentation for a more in-depth explanation of the arbiter
+
+
+# Imports necessary libraries
 import roslib; roslib.load_manifest('olinoboat')
 import rospy
 from std_msgs.msg import Int16
-import go_fast
-import go_short
 import time
 import matplotlib.pyplot as plt
 from math import pi
 
+# Imports necessary functions
+import go_fast
+import go_short
+
+# If you set this to True, think.py will try to make a graph of where it wants you to go
 plot_heading = True
 
 think_node = rospy.init_node("think")
@@ -17,8 +29,10 @@ go_short.init(think_node)
 
 think_command_pub = rospy.Publisher('desired_heading', Int16)
 
-x = [i*pi/180 for i in range(360)]
+if plot_heading == True:
+	x = [i*pi/180 for i in range(360)]
 
+# This is the 'think loop', which redecides which direction to go every cycle
 while not rospy.is_shutdown():
 	time.sleep(5)
 	fast_behavior = go_fast.suggested_heading
@@ -26,8 +40,9 @@ while not rospy.is_shutdown():
         desired_behavior = [fast*short for fast, short in zip(fast_behavior, short_behavior)]
 	desired_heading = desired_behavior.index(max(desired_behavior))
 	if desired_heading > 180: desired_heading -= 360
+	
 	think_command_pub.publish(Int16(desired_heading)))
-	rospy.loginfo("think.py suggested heading is: %f" %desired_heading)
+	rospy.loginfo("think.py chose this heading: %f degrees" %desired_heading)
 
 	if plot_heading == True:
 		plt1 = plt.subplot(1, 3, 1, projection='polar')
