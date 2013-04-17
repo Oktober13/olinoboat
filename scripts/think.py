a46db3fab63cdcc9e0db465ec102f3d4603ef126
Trevor <trevor@trevor-Latitude-E6400.(none)>
2013-04-17 17:29:27 -0400

    updated launch file to launch serial node

--- scripts/think.py
+++ scripts/think.py
@@ -31,6 +31,19 @@ think_command_pub = rospy.Publisher('desired_heading', Int16)
 
 if plot_heading == True:
 	x = [i*pi/180 for i in range(360)]
+	plt1 = plt.subplot(1, 3, 1, projection='polar')
+	plt1.set_theta_zero_location('N')
+	plt1.set_theta_direction(-1)
+
+	plt2 = plt.subplot(1, 3, 2, projection='polar')
+	plt2.set_theta_zero_location('N')
+	plt2.set_theta_direction(-1)
+
+	plt3 = plt.subplot(1, 3, 3, projection='polar')
+	plt3.set_theta_zero_location('N')
+	plt3.set_theta_direction(-1)
+	
+	plt.show
 
 # This is the 'think loop', which redecides which direction to go every cycle
 while not rospy.is_shutdown():
@@ -45,18 +58,7 @@ while not rospy.is_shutdown():
 	rospy.loginfo("think.py chose this heading: %f degrees" %desired_heading)
 
 	if plot_heading == True:
-		plt1 = plt.subplot(1, 3, 1, projection='polar')
-		plt1.set_theta_zero_location('N')
-		plt1.set_theta_direction(-1)
-
-		plt2 = plt.subplot(1, 3, 2, projection='polar')
-		plt2.set_theta_zero_location('N')
-		plt2.set_theta_direction(-1)
-
-		plt3 = plt.subplot(1, 3, 3, projection='polar')
-		plt3.set_theta_zero_location('N')
-		plt3.set_theta_direction(-1)
-
+		plt.cla
 		plt.subplot(1, 3, 2)
 		plt.polar(x, fast_behavior)
 		plt.subplot(1, 3, 1)
@@ -64,4 +66,8 @@ while not rospy.is_shutdown():
 		plt.subplot(1, 3, 3)
 		plt.polar(x, short_behavior)
 
-		plt.show()
+		plt.draw()
+
+
+
+
