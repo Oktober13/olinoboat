0e8c7f03d088520fd921707283c59c2a56b222e6
eschneider1992 <franz.eric.schneider@gmail.com>
2013-06-06 11:06:38 -0400

    Made auto_trim_sails.py subscribe and publish to the correct topics

--- CBI_sailbot_code/auto_trim_sails.py
+++ CBI_sailbot_code/auto_trim_sails.py
@@ -9,12 +9,8 @@
 # Imports necessary libraries
 import roslib; roslib.load_manifest('olinoboat')
 import rospy
-from std_msgs.msg import Int16
-
-# Imports necessary sailbot code
-from programming_tools import pid_controls
-from hardware import sensors
-from hardware import servos
+import pylab as pl
+from std_msgs.msg import UInt16
 
 # This function using PID control to get the boat pointed at the correct compass heading
 # This function updates every time the compass heading updates
@@ -23,51 +19,23 @@ from hardware import servos
 #		The Setpoint of the PID control is the command 'boat_heading.setPoint(desired_compass_heading)'
 #		The Process Variable is the compass data, seen in the command 'boat_heading.update(compass_heading)'
 #		The Controller Output, in this case, is called pid_command, and we use that to set our rudder angle
-def point_boat_at_target_on_compass_change():
-	compass_heading = sensors.compass.heading
-	rospy.loginfo("point_boat_at_target.py: Boat is pointing at %f degrees and should be at %i degrees" %(compass_heading, boat_heading.getPoint()))
+def auto_trim_sails(data):
+	wind_angle = data.data
+	rospy.loginfo("auto_trim_sails.py: recieved wind angle: %i" %wind_angle)
+	wind_angle = abs((wind_angle + 180) % 360 - 180)
+	rospy.loginfo("auto_trim_sails.py: simplified (0-180) wind angle: %i" %wind_angle)
+	points_of_sail = [0, 45, 60, 90, 135, 180]
+	sail_points = [0, 0.5, 2, 4, 8, 9]
+	sail_setting = pl.interp(wind_angle, points_of_sail, sail_points)
+	sail_angle = sail_setting*(1/1) # PUT CODE HERE THAT TRANSFORMS A 0-10 SCALE TO ACTUAL FULL-IN FULL-OUT ON THE SERVO
+	rospy.loginfo("auto_trim_sails.py: sail servo is being set to: %i" %sail_angle)
+	sail_pub.publish(UInt16(sail_angle))
 
-	pid_command = boat_heading.update(compass_heading)
-	rospy.loginfo('point_boat_at_target.py: pid command %i' %pid_command)
-
-	rudder_bound = 45	# This is, in degrees, the farthest that the rudder should go to either side. Can be reset here
-	rudder_angle = clamp(pid_command, -rudder_bound, rudder_bound)	# This works assuming the rudder has 'S = 0' and angle increasing CCW
-	# That way when the boat turns left (pid_command is negative) then the rudder turns clockwise, making a left turn
-
-	rospy.loginfo("point_boat_at_target.py: rudder angle is being set to: %i" %rudder_angle)
-	servos.rudder.set_position(rudder_angle)
-
-# This function is subscribed to the think code - as soon as our arbiter decides a new direction we set that as the Setpoint of our PID control loop
-def change_desired_heading(data):
-	global boat_heading
-	compass_heading = sensors.compass.heading
-	desired_compass_heading = (compass_heading + data.data) % 360
-	boat_heading.setPoint(desired_compass_heading)	# The PID setpoint is updated
-	rospy.loginfo("point_boat_at_target.py: Boat should reorient to compass heading %i" %desired_compass_heading)	
-
-def clamp(n, minn, maxn):
-    return max(min(maxn, n), minn)
-
-
-rospy.sleep(10)
-print 'init maintaing_point_boat_at_target node'
-current_node = rospy.init_node("point_boat_at_target_node",anonymous=True)
-sensors.init(current_node)
-servos.init(current_node)
+current_node = rospy.init_node("auto_trim_sails_node", anonymous=True)
 
 # This ties change_desired_heading() to the desired_heading() topic (this is published to by mission_publish.py)
-rospy.Subscriber("desired_heading", Int16, change_desired_heading)
+rospy.Subscriber("pwm_duration", UInt16, auto_trim_sails)
 
-# This ties point_boat_at_target_on_compass_change() to a change in compass data
-sensors.compass.set_callback(point_boat_at_target_on_compass_change)
-
-# You can tune the PID control by altering these constants
-# Tuning a PID controller is very useful, and helps the robot control the boat better
-# Some info about tuning can be found here, but googling will turn up more if this isn't useful to you
-#		https://controls.engin.umich.edu/wiki/index.php/PIDTuningClassical#Trial_and_Error
-# Essentially, making P control larger will get faster responses, but cause overshoot
-# Higher D control will help prevent overshoot
-# Higher I control will help if the boat is getting close to the desired angle but never quite gets there
-boat_heading = pid_controls.PID(1.0, 0.0, 0.0)
+sail_pub = rospy.Publisher('sail', UInt16)
 
 rospy.spin()
